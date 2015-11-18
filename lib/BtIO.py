9e8232c0b2bfead6dd0200bb6879bbee0c47e1c2
Dom Laetsch <dominik@Rambler.local>
2015-11-18 18:40:25 +0000

    fix format

--- lib/BtIO.py
+++ lib/BtIO.py
@@ -71,7 +71,7 @@ def checkBam(infile):
         output += line
     mapped_reads = mapped_reads_re.search(output).group(1)
     total_reads = total_reads_re.search(output).group(1)
-    print BtLog.status_d['11'] % (infile, mapped_reads, total_reads, '{0:.1%}'.format(mapped_reads/total_reads))
+    print BtLog.status_d['11'] % (infile, "{:,}".format(mapped_reads), "{:,}".format(total_reads), '{0:.1%}'.format(int(mapped_reads)/int(total_reads)))
     return total_reads, mapped_reads
 
 def readSam(infile, set_of_blobs):
