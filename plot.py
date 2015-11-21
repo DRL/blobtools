688a389d5849d5bae5ccfc53f52e984add668dde
Dom Laetsch <dominik@Rambler.local>
2015-11-21 12:55:53 +0000

    ...

--- plot.py
+++ plot.py
@@ -149,7 +149,7 @@ if __name__ == '__main__':
             out_f = "%s.%s" % (out_f, "label_" + "_".join(set([name for name in user_labels.values()])))
         out_f = "%s.%s.%s" % (out_f, min_length, taxrule)
         plotObj.out_f = out_f
-        plotObj.plotBlobs(cov_lib, info_flag)
+        #plotObj.plotBlobs(cov_lib, info_flag)
         info_flag = 0
-    plotObj.write_stats()
+    #plotObj.write_stats()
     plotObj.plotReadCov()
