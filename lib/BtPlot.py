cee0f56ce992c421a16352ea0365c3f7f7bfc860
Dom Laetsch <dominik@Rambler.local>
2015-11-21 15:54:58 +0000

    ...

--- lib/BtPlot.py
+++ lib/BtPlot.py
@@ -28,7 +28,7 @@ mat.rcParams['ytick.major.pad'] = '8'
 mat.rcParams['lines.antialiased'] = True
 
 FONTSIZE = 24
-COLOURMAP = "Paired2" # "Set1"
+COLOURMAP = "Spectral" # "Set1"
 BLACK, GREY, BGGREY, WHITE, DGREY = unicode('#262626'), unicode('#d3d3d3'), unicode('#F0F0F5'), unicode('#ffffff'), unicode('#3B3B3B')
 nullfmt = NullFormatter()
 
