a33a56e6b80a3cb89b08b84a2211ed9783dcf2d3
Dom Laetsch <dominik@Rambler.local>
2015-11-21 16:30:12 +0000

    ...

--- plot.py
+++ plot.py
@@ -146,7 +146,7 @@ if __name__ == '__main__':
         if (plotObj.title):
             plotObj.title = "%s.%s" % (plotObj.title, cov_lib)
 
-        out_f = "%s.%s.%s.%s.p%s" % (title, cov_lib, hist_type, rank, max_group_plot)
+        out_f = "%s.%s.%s.p%s" % (title, hist_type, rank, max_group_plot)
         if out_prefix:
             out_f = "%s.%s" % (out_prefix, out_f)
         if c_index:
