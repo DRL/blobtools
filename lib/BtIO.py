512a2791bd3326cec28b7e90ef9fe47915b8d114
Dom Laetsch <dominik@Rambler.local>
2015-12-03 13:59:10 +0000

    ..
    .

--- lib/BtIO.py
+++ lib/BtIO.py
@@ -96,7 +96,7 @@ def readSam(infile, set_of_blobs):
 
 def readBam(infile, set_of_blobs):
     reads_total, reads_mapped = checkBam(infile)
-    progress_unit = int(int(reads_mapped)/1000) +  # lazy fix
+    progress_unit = int(int(reads_mapped)/1000) + 1 # lazy fix
     base_cov_dict = {}
     read_cov_dict = {}
     cigar_match_re = re.compile(r"(\d+)M") # only gets digits before M's
