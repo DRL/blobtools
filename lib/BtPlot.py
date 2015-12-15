64ee1c975bf341a7a48f27d38753e7291522fe4b
Dom Laetsch <dominik@Rambler.local>
2015-12-15 13:02:26 +0000

    ...

--- lib/BtPlot.py
+++ lib/BtPlot.py
@@ -467,7 +467,7 @@ class PlotObj():
                 # unmapped (assembly)
                 reads_total = self.cov_libs_total_reads_dict[cov_lib]
                 reads_unmapped = reads_total - self.stats['all']['reads_mapped'][cov_lib]
-                print reads_total, reads_unmapped, self.stats['all']['reads_mapped']
+                print reads_total, reads_unmapped, self.stats['all']
                 if cov_lib in refcov_dict:
                     reads_total_ref = refcov_dict[cov_lib]['reads_total']
                     reads_mapped_ref = refcov_dict[cov_lib]['reads_mapped'] 
