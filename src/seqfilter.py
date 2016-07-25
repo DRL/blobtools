70c529da3e20787eb0244966c8fa3193c774fbdc
Dom Laetsch <dominik@Rambler.local>
2016-07-25 10:16:43 +0100

    ...

--- src/seqfilter.py
+++ src/seqfilter.py
@@ -43,7 +43,7 @@ if __name__ == '__main__':
         else:
             if (invert):
                 output.append(">%s\n%s\n" % (header, sequence))
-        BtLog.progress(len(output), len(output)/1000, len(itemsgi))
+        BtLog.progress(len(output), len(output)/1000, len(items))
     with open(out_f, "w") as fh:
         fh.write("".join(output))
 
