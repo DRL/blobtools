dd9a11712639e949d954dbb56e5dab983ceea806
Dominik R Laetsch <dom@dombadom.local>
2017-07-10 21:38:14 +0100

    Improvements to taxify
    - no takes any sort of TSV input to construct hits files

--- bloblib/taxify.py
+++ bloblib/taxify.py
@@ -9,6 +9,7 @@
     Options:
         -h --help                           show this
 
+    Options for similarity search input
         -f, --hit_file <FILE>               BLAST/Diamond similarity search result (TSV format).
                                                 Defaults assume "-outfmt '6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore'"
         -a, --hit_column_qseqid <INT>       Zero-based column of qseqid in similarity search result [default: 0]
@@ -17,13 +18,17 @@
                                                 Change if different format than (-outfmt '6')
         -c, --hit_column_score <INT>        Zero-based column of (bit)score in similarity search result [default: 11]
                                                 Change if different format than (-outfmt '6')
-    Based on taxid file
+    Options for TaxID mapping file
         -m, --taxid_mapping_file <FILE>     TaxID mapping file (contains seqid and taxid)
         -s, --map_col_sseqid <INT>          Zero-based column of sseqid in TaxID mapping file (it will search for sseqid in this column)
         -t, --map_col_taxid <INT>           Zero-based Column of taxid in TaxID mapping file (it will extract for taxid from this column)
 
-    Based on taxid
-        --taxid <INT>                       TaxID (must be integer)
+    Options for custom input
+        -i, --custom <FILE>                 File containing list of sequence IDs
+        -x, --custom_taxid <INT>            TaxID to assign to all sequence IDs in list
+        -v, --custom_score <FLOAT>          Score to assign to all sequence IDs in list
+
+    General
         -o, --out <PREFIX>                  Output prefix
 """
 
@@ -48,7 +53,9 @@ def main():
     map_f = args['--taxid_mapping_file']
     map_col_sseqid = args['--map_col_sseqid']
     map_col_taxid = args['--map_col_taxid']
-    custom_taxid = args['--taxid']
+    custom_f = args['--custom']
+    custom_taxid = args['--custom_taxid']
+    custom_score = args['--custom_score']
     prefix = args['--out']
 
     try:
